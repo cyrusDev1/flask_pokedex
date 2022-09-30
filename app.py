830fc28970bd688b96c10f5127a35a50eb6aee08
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-30 19:59:45 +0100

    add pokedex route

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from webbrowser import get
 from flask import Flask, render_template
 from pokemon import *
 from liveserver import LiveServer
@@ -6,10 +7,17 @@ import requests
 app = Flask(__name__)
 
 ls = LiveServer(app)
+pokemons = asyncio.run(all_pokemon())
 
 @app.route('/')
 def home():
-    pokemons = asyncio.run(all_pokemon())
     return ls.render_template('index.html', pokemons=pokemons)
 
-
+@app.route('/pokedex/<int:id>')
+def pokedex(id):
+    url_desc = pokemons[1].get("species").get('url')
+    desc = asyncio.run(one_pokedex(url_desc))
+    one_pokemon = {}
+    one_pokemon['image'] = pokemons[id].get("sprites").get("front_default")
+    one_pokemon['description'] = desc.get("flavor_text_entries")[1].get("flavor_text")
+    return ls.render_template('pokedex.html', one_pokemon=one_pokemon)
--- pokemon.py
+++ pokemon.py
@@ -11,8 +11,14 @@ async def pokemon(session, url):
 async def all_pokemon():
     async with aiohttp.ClientSession() as session:
         task = []
-        for id in range(1, 12):
+        for id in range(1, 13):
             url = f"https://pokeapi.co/api/v2/pokemon/{id}"
             task.append(asyncio.ensure_future(pokemon(session, url)))
         original_pokemon = await asyncio.gather(*task)
         return(original_pokemon)
+
+async def one_pokedex(url):
+    async with aiohttp.ClientSession() as session_:
+        async with session_.get(url) as response:
+            pokemon_ = await response.json()
+            return pokemon_
